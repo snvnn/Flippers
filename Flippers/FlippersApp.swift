d23272b541d1e80d89c1951ad6eba24fdd6ce7a0
snvnn <yunh1205@gmail.com>
2026-03-30 13:43:51 +0900

    Initial Commit

---
+++ Flippers/FlippersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippersApp.swift
+//  Flippers
+//
+//  Created by 윤현 on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FlippersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlippersTests/FlippersTests.swift
@@ -0,0 +1,19 @@
+//
+//  FlippersTests.swift
+//  FlippersTests
+//
+//  Created by 윤현 on 3/30/26.
+//
+
+import Testing
+@testable import Flippers
+
+struct FlippersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
